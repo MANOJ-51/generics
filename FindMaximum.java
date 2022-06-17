8798ec7e4aa0931e23c6795a761d0f01856a46d9
MANOJ-51 <m4anoj51@gmail.com>
2022-06-17 23:38:34 +0530

    added: method to find the maximum string length

--- FindMaximum.java
+++ FindMaximum.java
@@ -8,11 +8,18 @@ public class FindMaximum {
         object.integerMax(48, 24, 38);   //test case 1.1 maximum number at position 1
         object.integerMax(28, 56, 24);   //test case 1.2 maximum number at position 2
         object.integerMax(24, 34, 54);   //test case 1.3 maximum number at position 3
+        System.out.println(" ");
 
         //finding maximum float number
         object.floatMax(5.2f, 2.5f, 3.1f);  //test case 1.1 maximum number at position 1
         object.floatMax(2.2f, 7.2f, 4.5f);  //test case 1.1 maximum number at position 2
         object.floatMax(1.1f, 2.3f, 4.5f);  //test case 1.1 maximum number at position 3
+        System.out.println(" ");
+
+        //finding maximum string length
+        object.stringMax("Zam", "Manoj", "Sandeep");
+        object.stringMax("Sandeep", "Zam", "Manoj");
+        object.stringMax("Manoj", "Sandeep", "Zam");
 
     }
 
@@ -22,7 +29,7 @@ public class FindMaximum {
         if (number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0) {
             System.out.println(number1 + " Is Maximum ");
         } else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0) {
-            System.out.println(number2 + " Is Maximum ");
+            System.out.println(number2 + "  Is Maximum ");
         } else {
             System.out.println(number3 + " Is Maximum ");
         }
@@ -31,11 +38,22 @@ public class FindMaximum {
     //method to find maximum float number
     public void floatMax(Float number1, Float number2, Float number3) {
         if (number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0) {
-            System.out.println("test case 1.1 " + number1 + " Is Maximum at position 1");
+            System.out.println("test case 1.1 :- " + number1 + " Is Maximum at position 1");
         } else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0) {
-            System.out.println("test case 1.2 " + number2 + " Is Maximum at position 2");
+            System.out.println("test case 1.2 :- " + number2 + " Is Maximum at position 2");
         } else {
-            System.out.println("test case 1.3" + number3 + " Is Maximum at position 3");
+            System.out.println("test case 1.3:- " + number3 + " Is Maximum at position 3");
+        }
+    }
+
+    //method to find the maximum string length
+    public void stringMax(String string1, String string2, String string3) {
+        if (string1.compareTo(string2) > 0 && string1.compareTo(string3) > 0) {
+            System.out.println("test case 1.1 :-" + string1 + " Is Maximum at position 1");
+        } else if (string2.compareTo(string1) > 0 && string2.compareTo(string3) > 0) {
+            System.out.println("test case 1.2 :-" + string2 + " Is Maximum at position 2");
+        } else {
+            System.out.println("test case 1.3 :-" + string3 + " Is Maximum at position 3");
         }
     }
 
