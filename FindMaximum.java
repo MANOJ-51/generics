93c48e1464f8d622bbc197cf205fd7e6b4a376b1
MANOJ-51 <m4anoj51@gmail.com>
2022-06-19 20:29:02 +0530

    added: a method to print more parameters

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,48 +1,21 @@
-public class FindMaximum<M extends Comparable<M>> {
+import java.util.Arrays;
 
-    M variable1;
-    M variable2;
-    M variable3;
-
-    public FindMaximum(M variable1, M variable2, M variable3) {
-        this.variable1 = variable1;
-        this.variable2 = variable2;
-        this.variable3 = variable3;
-    }
+public class FindMaximum {
 
     public static void main(String[] args) {
         //displaying welcome message
         System.out.println(" Find Maximum Number using Generics ");
 
-        //creating object and finding maximum integer number
-        findMaximum(48, 24, 38);
-        System.out.println(" ");
-
-        //finding maximum float number
-        findMaximum(5.2f, 2.5f, 3.1f);
-        System.out.println(" ");
-
-        //finding maximum string length
-        findMaximum("Zam", "Manoj", "Siva");
+        System.out.println("Maximum values of integer is :- " + findMaximum(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        System.out.println("Maximum values of float is :- " + findMaximum(1.1, 1.2, 1.3, 1.4, 1.5));
+        System.out.println("Maximum length of string is :- " + findMaximum("manoj", "sander", "rani", "hari"));
 
     }
 
-    //method to find maximum by using generics
-    public static <M extends Comparable<M>> M findMaximum(M variable1, M variable2, M variable3) {
-        M max = variable3;
-
-        //comparing numbers using compare to method
-        if (variable1.compareTo(variable2) > 0 && variable1.compareTo(max) > 0) {
-            max = variable1;
-            System.out.println(max + " :- Is Maximum ");
-        } else if (variable2.compareTo(variable1) > 0 && variable2.compareTo(max) > 0) {
-            max = variable2;
-            System.out.println(max + " :- Is Maximum ");
-        } else {
-            max = variable3;
-            System.out.println(max + " :- Is Maximum ");
-        }
-        return max;
+    //using array method to print more numbers
+    public static <M> M findMaximum(M... a) {
+        Arrays.sort(a);
+        return a[a.length - 1];
     }
 
 }
