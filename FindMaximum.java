0029c8eae42b5098e634a69e6937fa03a407e23a
MANOJ-51 <m4anoj51@gmail.com>
2022-06-19 21:11:12 +0530

    added: method to print maximum

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,21 +1,72 @@
 import java.util.Arrays;
 
-public class FindMaximum {
+public class FindMaximum<M extends Comparable<M>> {
+    // Constant variables
+    private final M VARIABLE1;
+    private final M VARIABLE2;
+    private final M VARIABLE3;
+    private final M VARIABLE4;
+    private final M VARIABLE5;
+
+
+    // Constructor
+
+    public FindMaximum(M variable1, M variable2, M variable3, M variable4, M variable5) {
+        this.VARIABLE1 = variable1;
+        this.VARIABLE2 = variable2;
+        this.VARIABLE3 = variable3;
+        this.VARIABLE4 = variable4;
+        this.VARIABLE5 = variable5;
+    }
+
+    // Method by using generics
+    public static <M extends Comparable<M>> M testMaximum(M variable1, M variable2, M variable3, M variable4, M variable5) {
+
+        M max = variable5;
+        // comparing numbers using compareTo method.
+        if (variable1.compareTo(variable2) > 0 && variable1.compareTo(variable3) > 0 && variable1.compareTo(variable4) > 0 && variable1.compareTo(max) > 0) {
+            max = variable1;
+        } else if (variable2.compareTo(variable1) > 0 && variable2.compareTo(variable3) > 0 && variable2.compareTo(variable4) > 0 && variable2.compareTo(max) > 0) {
+            max = variable2;
+        } else if (variable3.compareTo(variable1) > 0 && variable3.compareTo(variable2) > 0 && variable3.compareTo(variable4) > 0 && variable3.compareTo(max) > 0) {
+            max = variable3;
+        } else if (variable4.compareTo(variable1) > 0 && variable4.compareTo(variable2) > 0 && variable4.compareTo(variable3) > 0 && variable4.compareTo(max) > 0) {
+            max = variable4;
+        } else {
+            max = variable5;
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
         //displaying welcome message
         System.out.println(" Find Maximum Number using Generics ");
 
-        System.out.println("Maximum values of integer is :- " + findMaximum(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        System.out.println("Maximum values of float is :- " + findMaximum(1.1, 1.2, 1.3, 1.4, 1.5));
-        System.out.println("Maximum length of string is :- " + findMaximum("manoj", "sander", "rani", "hari"));
+        //creating object and finding maximum integer number
+        FindMaximum objectInteger = new FindMaximum(1, 2, 3, 4, 5);
+        objectInteger.testMaximum();
+        System.out.println(" ");
 
+        //finding maximum float number
+        FindMaximum objectFloat = new FindMaximum(1.1, 1.2, 1.3, 1.4, 1.5);
+        objectFloat.testMaximum();
+        System.out.println(" ");
+
+        //finding maximum string length
+        FindMaximum objectString = new FindMaximum("manoj", "sandeep", "rani", "hari", "sujatha");
+        objectString.testMaximum();
     }
 
-    //using array method to print more numbers
-    public static <M> M findMaximum(M... a) {
-        Arrays.sort(a);
-        return a[a.length - 1];
+
+    // Creating print maximum function
+
+    public M testMaximum() {
+        M findMax = FindMaximum.testMaximum(VARIABLE1, VARIABLE2, VARIABLE3, VARIABLE4, VARIABLE5);
+        printMax(findMax); //calling printMaximum method.
+        return findMax;
     }
 
+    public void printMax(M max) {
+        System.out.println("\nMAXIMUM IS :- " + max);
+    }
 }
