aca1d83309f1fd5a71a26b2acab3ed693fb77dbb
MANOJ-51 <m4anoj51@gmail.com>
2022-06-19 20:04:20 +0530

    added: static method to find maximum number using three variables

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,41 +1,46 @@
-public class FindMaximum {
+public class FindMaximum<M extends Comparable<M>> {
+
+    M variable1;
+    M variable2;
+    M variable3;
+
+    public FindMaximum(M variable1, M variable2, M variable3) {
+        this.variable1 = variable1;
+        this.variable2 = variable2;
+        this.variable3 = variable3;
+    }
+
     public static void main(String[] args) {
         //displaying welcome message
         System.out.println(" Find Maximum Number using Generics ");
 
         //creating object and finding maximum integer number
-        FindMaximum object = new FindMaximum();
-        object.findMaximum(48, 24, 38);   //test case 1.1 maximum number at position 1
-        object.findMaximum(28, 56, 24);   //test case 1.2 maximum number at position 2
-        object.findMaximum(24, 34, 54);   //test case 1.3 maximum number at position 3
+        findMaximum(48, 24, 38);
         System.out.println(" ");
 
         //finding maximum float number
-        object.findMaximum(5.2f, 2.5f, 3.1f);  //test case 1.1 maximum number at position 1
-        object.findMaximum(2.2f, 7.2f, 4.5f);  //test case 1.1 maximum number at position 2
-        object.findMaximum(1.1f, 2.3f, 4.5f);  //test case 1.1 maximum number at position 3
+        findMaximum(5.2f, 2.5f, 3.1f);
         System.out.println(" ");
 
         //finding maximum string length
-        object.findMaximum("Zam", "Manoj", "Sandeep");
-        object.findMaximum("Sandeep", "Zam", "Manoj");
-        object.findMaximum("Manoj", "Sandeep", "Zam");
+        findMaximum("Zam", "Manoj", "Siva");
 
     }
+
     //method to find maximum by using generics
-    public <M extends Comparable <M>> M findMaximum(M variable1,M variable2,M variable3){
-        M max =variable3;
+    public static <M extends Comparable<M>> M findMaximum(M variable1, M variable2, M variable3) {
+        M max = variable3;
 
         //comparing numbers using compare to method
-        if (variable1.compareTo(variable2)>0&&variable1.compareTo(max)>0){
-            max=variable1;
-            System.out.println("test case 1.1 :- "+max+" Is Maximum ");
-        } else if (variable2.compareTo(variable1)>0&&variable2.compareTo(max)>0) {
-            max=variable2;
-            System.out.println("test case 1.2 :- "+max+" Is Maximum ");
-        }else {
-            max=variable3;
-            System.out.println("test case 1.3 :- "+max+" Is Maximum ");
+        if (variable1.compareTo(variable2) > 0 && variable1.compareTo(max) > 0) {
+            max = variable1;
+            System.out.println(max + " :- Is Maximum ");
+        } else if (variable2.compareTo(variable1) > 0 && variable2.compareTo(max) > 0) {
+            max = variable2;
+            System.out.println(max + " :- Is Maximum ");
+        } else {
+            max = variable3;
+            System.out.println(max + " :- Is Maximum ");
         }
         return max;
     }
